c2e17998aa337e2117ae499da667e7430b6133b7
tl-bright <2505044623@qq.com>
2023-03-19 23:26:03 +0800

    linklist question

--- src/arith/data/node.ts
+++ src/arith/data/node.ts
@@ -10,8 +10,8 @@ export class ListNode {
 const head = new ListNode(6);
 const node1 = new ListNode(9);
 const node2 = new ListNode(13);
-const node3 = new ListNode(8);
-const node4 = new ListNode(19);
+const node3 = new ListNode(19);
+const node4 = new ListNode(20);
 const node5 = new ListNode(26);
 head.next = node1;
 node1.next = node2;
@@ -19,4 +19,20 @@ node2.next = node3;
 node3.next = node4;
 node4.next = node5;
 
-export { head };
+const node = new ListNode(10);
+const n1 = new ListNode(30);
+const n2 = new ListNode(33);
+const n3 = new ListNode(36);
+node.next = n1;
+n1.next = n2;
+n2.next = n3;
+
+const cycle = new ListNode(37);
+const q1 = new ListNode(6);
+const q2 = new ListNode(16);
+const q3 = new ListNode(7);
+cycle.next = q1;
+q1.next = q2;
+q2.next = q3;
+q3.next = q1;
+export { head, node, cycle };
---
+++ src/arith/medium/reverse-k-group.ts
@@ -0,0 +1,34 @@
+import { head, ListNode } from "../data/node";
+import { getSize, print, reverseLinklist } from "../utils/linklist.util";
+
+/**
+ * 来源：牛客网 TOP101 题号: BM3
+ * 链表中的节点每 k 个一组翻转
+ * @param head ListNode类
+ * @param k int整型
+ * @return ListNode类
+ */
+function reverseKGroup(head: ListNode, k: number): ListNode {
+  // write code here
+  if (head === null) return head;
+  // 链表长度
+  const size = getSize(head);
+  let dummy = new ListNode(-1);
+  dummy.next = head;
+  let curr: ListNode | null = dummy;
+  for (let i = 0; i < Math.floor(size / k); i++) {
+    const pre = curr;
+    for (let j = 0; j < k; j++) curr = curr.next as ListNode;
+    // 记录下一组第一个节点且断开与下组的连接
+    const next = curr.next;
+    curr.next = null;
+    const [head, tail] = reverseLinklist(pre.next);
+    pre.next = head;
+    if (tail) tail.next = next;
+    curr = tail as ListNode;
+  }
+  return dummy.next;
+}
+
+const node = reverseKGroup(head, 2);
+print(node)
