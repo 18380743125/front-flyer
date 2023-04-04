c16134885a577d8dff69baef90fa088aeb2b612f
tl-bright <2505044623@qq.com>
2023-04-04 23:52:59 +0800

    LFU

---
+++ src/arith/hard/LFU.ts
@@ -0,0 +1,174 @@
+type Node = CacheNode | FreqNode;
+
+// 缓存节点
+export class CacheNode {
+  freq: number;
+  key?: number;
+  value?: number;
+  prev: CacheNode | null;
+  next: CacheNode | null;
+  constructor(freq: number, key?: number, value?: number) {
+    this.freq = freq;
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+// 频次节点
+export class FreqNode {
+  freq: number;
+  cacheHead: CacheNode;
+  cacheTail: CacheNode;
+  prev: FreqNode | null;
+  next: FreqNode | null;
+  constructor(freq: number) {
+    this.freq = freq;
+    this.cacheHead = new CacheNode(0);
+    this.cacheTail = new CacheNode(0);
+    this.cacheHead.next = this.cacheTail;
+    this.cacheTail.prev = this.cacheHead;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+// 设计 LFU 结构
+export class LFUCache {
+  capacity: number;
+  size: number;
+  freqHead: FreqNode;
+  freqTail: FreqNode;
+  cacheMap: Record<number, CacheNode>;
+  freqMap: Record<number, FreqNode>;
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.size = 0;
+    this.freqHead = new FreqNode(0);
+    this.freqTail = new FreqNode(0);
+    this.freqHead.next = this.freqTail;
+    this.freqTail.prev = this.freqHead;
+    this.cacheMap = {};
+    this.freqMap = {};
+  }
+
+  get(key: number): number {
+    const node = this.cacheMap[key] || null;
+    if (node !== null) {
+      this.moveToNextFreqNode(node);
+      return node.value as number;
+    }
+    return -1;
+  }
+
+  put(key: number, value: number) {
+    let node = this.cacheMap[key] || null;
+    if (node !== null) {
+      node.value = value;
+      this.moveToNextFreqNode(node);
+    } else {
+      if (this.size === this.capacity) {
+        this.removeLowUseCacheNode();
+      } else {
+        this.size++;
+      }
+      node = new CacheNode(1, key, value);
+      const freqNode = this.getFreqNode(1, this.freqTail);
+      this.appendAfter(node, freqNode.cacheHead);
+      this.cacheMap[key] = node;
+    }
+  }
+
+  removeLowUseCacheNode() {
+    const node = this.freqTail.prev?.cacheTail?.prev as CacheNode;
+    this.removeNode(node);
+    delete this.cacheMap[node.key as number];
+    this.removeFreqNodeWhenEmpty(node.freq);
+  }
+
+  moveToNextFreqNode(node: CacheNode) {
+    const freq = node.freq;
+    const freqNode = this.getFreqNode(freq + 1, this.freqMap[freq]);
+    this.removeNode(node);
+    this.appendAfter(node, freqNode.cacheHead);
+    node.freq = freq + 1;
+    this.removeFreqNodeWhenEmpty(freq);
+  }
+
+  getFreqNode(freq: number, target: FreqNode) {
+    let freqNode = this.freqMap[freq] || null;
+    if (freqNode === null) {
+      freqNode = new FreqNode(freq);
+      this.insertBefore(freqNode, target);
+      this.freqMap[freq] = freqNode;
+    }
+    return freqNode;
+  }
+
+  removeFreqNodeWhenEmpty(freq: number) {
+    const freqNode = this.freqMap[freq] || null;
+    if (freqNode === null) return;
+    if (freqNode.cacheHead.next === freqNode.cacheTail) {
+      this.removeNode(freqNode);
+      delete this.freqMap[freq];
+    }
+  }
+
+  insertBefore(node: Node, target: Node) {
+    const targetPrev = target.prev as Node;
+    node.next = target;
+    target.prev = node;
+    node.prev = targetPrev;
+    targetPrev.next = node;
+  }
+
+  appendAfter(node: Node, target: Node) {
+    const targetNext = target.next as Node;
+    node.prev = target;
+    target.next = node;
+    node.next = targetNext;
+    targetNext.prev = node;
+  }
+
+  removeNode(node: Node) {
+    const nodePrev = node.prev as Node;
+    const nodeNext = node.next as Node;
+    nodePrev.next = nodeNext;
+    nodeNext.prev = nodePrev;
+  }
+}
+
+/**
+ * 设计 LFU 缓存结构
+ * @param operators number[][]
+ * @param capacity 容量
+ * @return number[]
+ */
+export function LFU(operators: number[][], capacity: number): number[] {
+  // write code here
+  const lfu = new LFUCache(capacity);
+  const result: number[] = [];
+  for (const item of operators) {
+    if (item[0] === 1) {
+      lfu.put(item[1], item[2]);
+    } else {
+      const value = lfu.get(item[1]);
+      result.push(value);
+    }
+  }
+  return result;
+}
+
+const operators = [
+  [1, 1, 1],
+  [1, 2, 2],
+  [1, 3, 2],
+  [1, 2, 4],
+  [1, 3, 5],
+  [2, 2],
+  [1, 4, 4],
+  [2, 1],
+];
+const result = LFU(operators, 3);
+console.log(result);
--- src/arith/hard/LRU.ts
+++ src/arith/hard/LRU.ts
@@ -1,107 +1,107 @@
 // 双向链表节点
 export class Node {
-  public key: string
-  public val: number
-  public pre: Node | null
-  public next: Node | null
+  public key: string;
+  public val: number;
+  public pre: Node | null;
+  public next: Node | null;
   constructor(key: string, val: number) {
-    this.key = key
-    this.val = val
-    this.pre = null
-    this.next = null
+    this.key = key;
+    this.val = val;
+    this.pre = null;
+    this.next = null;
   }
 }
 
-class LRU {
-  private capacity: number
-  private size: number
-  private obj: Record<string, Node>
-  public head: Node
-  private tail: Node
+export class LRU {
+  private capacity: number;
+  private size: number;
+  private obj: Record<string, Node>;
+  public head: Node;
+  private tail: Node;
   constructor(capacity: number) {
-    this.size = 0
-    this.capacity = capacity
-    this.obj = {}
-    this.head = new Node('head', -1)
-    this.tail = new Node('tail', -1)
-    this.head.next = this.tail
-    this.tail.pre = this.head
+    this.size = 0;
+    this.capacity = capacity;
+    this.obj = {};
+    this.head = new Node("head", -1);
+    this.tail = new Node("tail", -1);
+    this.head.next = this.tail;
+    this.tail.pre = this.head;
   }
 
   get(key: string) {
-    const node = this.obj[key] || null
-    this.moveToHead(node)
-    if (node === null) return -1
-    else return node.val
+    const node = this.obj[key] || null;
+    this.moveToHead(node);
+    if (node === null) return -1;
+    else return node.val;
   }
 
   set(key: string, val: number) {
-    let node = this.obj[key] || null
+    let node = this.obj[key] || null;
     if (node !== null) {
-      node.val = val
-      this.moveToHead(node)
+      node.val = val;
+      this.moveToHead(node);
     } else {
-      node = new Node(key, val)
-      this.obj[key] = node
+      node = new Node(key, val);
+      this.obj[key] = node;
       if (this.capacity === this.size) {
         // 容量已满, 移出最近最少使用
-        this.removeLastNode()
-        this.size--
+        this.removeLastNode();
+        this.size--;
       }
-      this.size++
-      this.insertHead(node)
+      this.size++;
+      this.insertHead(node);
     }
   }
 
   // 插入到头部
   insertHead(node: Node) {
-    node.pre = this.head
-    node.next = this.head.next
+    node.pre = this.head;
+    node.next = this.head.next;
     if (this.head.next?.pre) {
-      this.head.next.pre = node
+      this.head.next.pre = node;
     }
-    this.head.next = node
+    this.head.next = node;
   }
 
   // 移动节点到头部
   moveToHead(node: Node) {
-    if (node === null) return
-    this.removeNode(node)
-    this.insertHead(node)
+    if (node === null) return;
+    this.removeNode(node);
+    this.insertHead(node);
   }
 
   // 移除最后一个节点
   removeLastNode() {
-    if (this.head.next === this.tail) return
-    const node = this.tail.pre as Node
-    this.removeNode(node)
-    delete this.obj[node.key]
+    if (this.head.next === this.tail) return;
+    const node = this.tail.pre as Node;
+    this.removeNode(node);
+    delete this.obj[node.key];
   }
 
   // 移除节点
   removeNode(node: Node | null) {
-    if (node === null) return
+    if (node === null) return;
     if (node.pre) {
-      node.pre.next = node.next
+      node.pre.next = node.next;
     }
     if (node.next) {
-      node.next.pre = node.pre
+      node.next.pre = node.pre;
     }
-    node.pre = null
-    node.next = null
+    node.pre = null;
+    node.next = null;
   }
 }
 
-const lru = new LRU(2)
+const lru = new LRU(2);
 
 // 1 -1 -1 3 4
-lru.set('1', 1)
-lru.set('2', 2)
-console.log(lru.get('1'))
-lru.set('3', 3)
+lru.set("1", 1);
+lru.set("2", 2);
+console.log(lru.get("1"));
+lru.set("3", 3);
 
-console.log(lru.get('2'))
-lru.set('4', 4)
-console.log(lru.get('1'))
-console.log(lru.get('3'))
-console.log(lru.get('4'))
+console.log(lru.get("2"));
+lru.set("4", 4);
+console.log(lru.get("1"));
+console.log(lru.get("3"));
+console.log(lru.get("4"));
